fd5c152170fc2a02c8d18305fd40d808c398cb60
Camilia <mzalicam@gmail.com>
2019-05-28 11:12:30 -0400

    set up routing and initial files

---
+++ src/components/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function About() {
+  return (
+    <React.Fragment>
+      <div className="jumbotron-fluid bg-dark text-light text-center">
+        <h1>Welcome to Pupster</h1>
+        <h2>Discover and search for your new doggy friends</h2>
+      </div>
+      <main>
+        <h2>The purpose of Pupster</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis deleniti distinctio architecto iste. Velit quaerat repellat eligendi asperiores debitis inventore molestiae! Mollitia inventore voluptatum molestiae cumque nisi expedita facere quae.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur rem cum similique doloremque cumque nulla quos ad pariatur cupiditate, maiores accusamus quaerat porro aut. Iusto voluptate dolorem reiciendis? Laboriosam, et.
+        </p>
+      </main>
+    </React.Fragment>
+  )
+}
+
+export default About;
---
+++ src/components/Discover.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Discover() {
+  return (
+    <div className="bg-dark text-light">
+      <h1>Discover</h1>
+    </div>
+  )
+}
+
+export default Discover;
